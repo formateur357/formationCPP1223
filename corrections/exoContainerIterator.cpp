608f08d21a11cd15d50050b5e2c60f4e120cf37a
moguy93 <guymorgan@live.fr>
2023-12-05 14:44:19 +0100

    add correction structure

---
+++ corrections/exoContainerIterator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+
+// Définition de la structure Livre
+struct Livre
+{
+};
+
+// Affiche les détails d'un livre
+void afficherLivre(const Livre &livre)
+{
+}
+
+int main()
+{
+    // Partie 1: Utilisation de vector et map ------------------------------------------------------------------------
+
+    // Ajout de livres dans le vector
+
+    // Affichage des livres en utilisant un itérateur
+
+    // Création d'une map pour mapper ID de livre à l'objet Livre
+
+    // Recherche d'un livre par ID
+
+    // Gestion des emprunts
+
+    // Partie 2: Opérations avancées et algorithmes STL --------------------------------------------------------------
+
+    // Tri du vector de livres par titre
+
+    // Filtrage des livres d'un auteur spécifique
+
+    // Utilisation de unordered_map pour regrouper les livres par auteur
+
+    // Suggestion de livres basés sur une plage de titres
+
+    return 0;
+}
