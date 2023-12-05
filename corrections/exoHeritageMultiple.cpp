7312c737226c35ad83049a4c2a1b5eac2f711c4a
moguy93 <guymorgan@live.fr>
2023-12-05 09:34:22 +0100

    prepare correction

--- corrections/exoHeritageMultiple.cpp
+++ corrections/exoHeritageMultiple.cpp
@@ -35,3 +35,39 @@ int main()
     // TODO: Effectuez le casting et appelez displayInfo() pour Camera et Speaker
     // TODO: Appeler displayInfo sur myPhone
 }
+
+// Partie 2 -------------------------------------------------------------------------------------------------
+
+// class GPS : public MultimediaDevice
+// {
+// public:
+//     void displayInfo() override { std::cout << "GPS functionality\n"; }
+// };
+
+// class MusicPlayer : public MultimediaDevice
+// {
+// public:
+//     void displayInfo() override { std::cout << "Music Player functionality\n"; }
+// };
+
+// class AdvancedSmartPhone : public SmartPhone, public GPS, public MusicPlayer
+// {
+// public:
+//     void displayInfo() override
+//     {
+//         // TODO: Intégrez les informations de toutes les classes de base
+//     }
+
+//     void activateFeature(const std::string &feature)
+//     {
+//         // TODO: Activez la fonctionnalité correspondante
+//     }
+// };
+
+// int main()
+// {
+
+//     AdvancedSmartPhone myAdvancedPhone;
+//     // TODO: Polymorphisme avec MultimediaDevice* et appels de displayInfo()
+//     // TODO: Testez activateFeature() avec différents paramètres
+// }
