c7ee020cc77f0e5f17ed427051b2cdfb5a31f45f
moguy93 <guymorgan@live.fr>
2023-12-06 14:04:47 +0100

    add exemple

---
+++ exemples/exempleBouclesIntervalle.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    for (auto i : v)
+    {
+        std::cout << i << "";
+    }
+
+    return 0;
+}
