4993bee6f6593787c2bdf22ad02d7af2fcd348f2
moguy93 <guymorgan@live.fr>
2023-12-06 12:03:55 +0100

    add installation

--- corrections/exoBoost.cpp
+++ corrections/exoBoost.cpp
@@ -27,43 +27,43 @@
 
 // -----------------------------------------------------------
 // Exo 2 => FileSystem
-#include <boost/filesystem.hpp>
-#include <iostream>
-#include <fstream>
+// #include <boost/filesystem.hpp>
+// #include <iostream>
+// #include <fstream>
 
-using namespace std;
-using namespace boost;
+// using namespace std;
+// using namespace boost;
 
-int main()
-{
-    // Creation d'un repertoire
-    filesystem::path dir("test_directory");
-    filesystem::create_directory(dir);
+// int main()
+// {
+//     // Creation d'un repertoire
+//     filesystem::path dir("test_directory");
+//     filesystem::create_directory(dir);
 
-    // Verification de l'existence du repertoire
-    if (filesystem::exists(dir))
-    {
-        cout << "Repertoire cree avec succes." << endl;
+//     // Verification de l'existence du repertoire
+//     if (filesystem::exists(dir))
+//     {
+//         cout << "Repertoire cree avec succes." << endl;
 
-        // Creation d'un fichier du repertoire
-        filesystem::path file = dir / "exemple.txt";
-        ofstream fileStream(file.string());
-        if (fileStream.is_open())
-        {
-            fileStream << "Exemple de texte";
-            fileStream.close();
-        }
+//         // Creation d'un fichier du repertoire
+//         filesystem::path file = dir / "exemple.txt";
+//         ofstream fileStream(file.string());
+//         if (fileStream.is_open())
+//         {
+//             fileStream << "Exemple de texte";
+//             fileStream.close();
+//         }
 
-        // Affichage du chemin absolu du fichier
-        cout << "Chemin du fichier cree : " << filesystem::absolute(file).string() << endl;
-    }
-    else
-    {
-        cout << "Impossible de creer le repertoire." << endl;
-    }
+//         // Affichage du chemin absolu du fichier
+//         cout << "Chemin du fichier cree : " << filesystem::absolute(file).string() << endl;
+//     }
+//     else
+//     {
+//         cout << "Impossible de creer le repertoire." << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 // -----------------------------------------------------------
 // Exo 3 => Regex
@@ -73,25 +73,57 @@ int main()
 
 // bool validateEmail(const std::string &email)
 // {
+//     const boost::regex pattern(R"(^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$)");
+//     return boost::regex_match(email, pattern);
 // }
 
 // int main()
 // {
+//     std::string email;
+//     std::cout << "Entrez une adresse email pour validation : ";
+//     std::cin >> email;
+
+//     if (validateEmail(email))
+//     {
+//         std::cout << "Adresse email valide." << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Adresse email invalide." << std::endl;
+//     }
 
 //     return 0;
 // }
 
 // -----------------------------------------------------------
 // Exo 4 => Format
-// #include <boost/format.hpp>
-// #include <iostream>
-// #include <string>
+#include <boost/format.hpp>
+#include <iostream>
+#include <string>
 
-// int main()
-// {
+using namespace std;
 
-//     return 0;
-// }
+int main()
+{
+    string nom, prenom;
+    int age;
+
+    cout << "Entrez votre nom : ";
+    cin >> nom;
+
+    cout << "Entrez votre prenom : ";
+    cin >> prenom;
+
+    cout << "Entrez votre age : ";
+    cin >> age;
+
+    boost::format formatter("Bonjour, %1% %2%. Vous avez %3% ans.");
+    formatter % prenom % nom % age;
+    string message = formatter.str();
+
+    cout << message << endl;
+    return 0;
+}
 
 // -----------------------------------------------------------
 // Exo 5 => FileSystem 2
