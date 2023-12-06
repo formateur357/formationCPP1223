c451b5089f7f020c6f3614dc2e5a2d0a1f6b4d94
moguy93 <guymorgan@live.fr>
2023-12-06 10:10:56 +0100

    add correction file

---
+++ corrections/exoBoost.cpp
@@ -0,0 +1,67 @@
+// -----------------------------------------------------------
+// Exo 1 => Lexical_cast
+#include <boost/lexical_cast.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+    return 0;
+}
+
+// -----------------------------------------------------------
+// Exo 2 => FileSystem
+#include <boost/filesystem.hpp>
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
+
+// -----------------------------------------------------------
+// Exo 3 => Regex
+#include <boost/regex.hpp>
+#include <iostream>
+#include <string>
+
+bool validateEmail(const std::string &email)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
+
+// -----------------------------------------------------------
+// Exo 4 => Format
+#include <boost/format.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+    return 0;
+}
+
+// -----------------------------------------------------------
+// Exo 5 => FileSystem 2
+#include <boost/filesystem.hpp>
+#include <iostream>
+
+void searchFiles(const boost::filesystem::path &root, const std::string &extension)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
+
+// -----------------------------------------------------------
