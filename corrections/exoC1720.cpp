1618e66d175852fe85fff8933399031fa50c84bf
moguy93 <guymorgan@live.fr>
2023-12-07 17:04:43 +0100

    add eval

--- corrections/exoC1720.cpp
+++ corrections/exoC1720.cpp
@@ -28,4 +28,73 @@ using namespace std;
 
 /////Exo 2
 
+// template <typename T>
+// concept Numeric = is_integral<T>::value || is_floating_point<T>::value;
+
+// template <Numeric T>
+// T average(T a, T b)
+// {
+//     return (a + b) / static_cast<T>(2);
+// }
+
+// int main()
+// {
+//     int num1 = 5, num2 = 7;
+
+//     double double1 = 2.5, double2 = 1.5;
+
+//     int average1 = average(num1, num2);
+//     double average2 = average(double1, double2);
+
+//     cout << "Average (int) : " << average1 << endl;
+//     cout << "Average (double) : " << average2 << endl;
+
+//     return 0;
+// }
+
 /////Exo 3
+
+#include <iostream>
+#include <variant>
+#include <string>
+
+using MyVariant = std::variant<int, std::string, bool>;
+
+// Overload for int
+void visitVariant(const int &value)
+{
+    std::cout << "Integer: " << value << std::endl;
+}
+
+// Overload for std::string
+void visitVariant(const std::string &value)
+{
+    std::cout << "String: " << value << std::endl;
+}
+
+// Overload for bool
+void visitVariant(const bool &value)
+{
+    std::cout << "Bool: " << std::boolalpha << value << std::endl;
+}
+
+int main()
+{
+    MyVariant data = "Hello, C++";
+
+    std::visit([](const auto &value)
+               { visitVariant(value); },
+               data);
+
+    data = true;
+    std::visit([](const auto &value)
+               { visitVariant(value); },
+               data);
+
+    data = 23;
+    std::visit([](const auto &value)
+               { visitVariant(value); },
+               data);
+
+    return 0;
+}
