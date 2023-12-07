398b2985e8c60e2cf0b45ec7e9912a4eb7b7cbf6
moguy93 <guymorgan@live.fr>
2023-12-07 10:06:06 +0100

    add correction

---
+++ corrections/exoGenericite.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept> // Pour la gestion des exceptions
+
+// 1. Créez une classe générique Calculator qui prend un paramètre de type initial (T) lors de son instanciation.
+
+// 2. Implémentez les méthodes d'opération de base.
+
+// Addition
+
+// Soustraction
+
+// Multiplication
+
+// Division
+
+// Assurez-vous de gérer le cas de division par zéro.
+
+// 3. Ajoutez des méthodes spécifiques pour les types std::string et std::vector.
+
+// Concaténation (spécifique à std::string)
+
+// Ajout (spécifique à std::vector)
+
+// 4. Affichez le résultat à l'écran.
+
+int main()
+{
+    // 5. Dans la fonction main, créez des instances de Calculator avec différents types de données.
+
+    // Calculator pour les entiers
+
+    // Calculator pour les chaînes de caractères
+
+    // Calculator pour les vecteurs d'entiers
+
+    // 6. Affichez les résultats des opérations.
+
+    // 7. Gérez les exceptions et affichez des messages d'erreur appropriés.
+
+    return 0;
+}
